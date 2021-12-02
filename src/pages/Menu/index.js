af7559afbb8884a243a96c95552ad268a42d2f9d
M-Asad-khan <asad.khan@devbox.co>
2021-12-02 23:16:44 +0500

    slider design in menu page

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import Slider from "@material-ui/core/Slider";
 function Menu() {
+  const [value, setValue] = useState(false);
+
+  const sliderPosition = (e, val) => {
+    if (val >= 80) {
+      setValue(true);
+    } else {
+      setValue(false);
+    }
+  };
   return (
-    <div>
-      <div>Menu</div>
+    <div className="bg-image12">
+      <div className="menuBackColor"></div>
+      <Slider
+        size="small"
+        defaultValue={70}
+        aria-label="Small"
+        onChange={sliderPosition}
+        className={value ? "sliderMargin" : ""}
+      />
     </div>
   );
 }
