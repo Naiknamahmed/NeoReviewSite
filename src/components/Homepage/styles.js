2df7279c4dd2a81cfdfa8e7a2b4a96c90410cce1
KhadeejaSaeed <khadijasaeed17@yahoo.com>
2022-03-10 22:20:43 +0500

    Refactored code

---
+++ src/components/Homepage/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    container : {
+        paddingTop:'4%',
+    },
+    wrapper : {
+        display:'flex',
+        justifyContent:'space-evenly',
+        alignItems:'baseline',
+    },
+    font : {
+        fontWeight:"bold",
+    }
+}));
